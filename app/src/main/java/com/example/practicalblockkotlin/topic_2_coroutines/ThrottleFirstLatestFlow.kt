b262ff6b8e5f7ca0a06b672c0e9cc781112c37d1
Dmitriy Zakharich <zakharichd@yandex.ru>
2024-09-03 01:14:37 +0300

    Fixed the extension function Flow.throttleFirst

--- app/src/main/java/com/example/practicalblockkotlin/topic_2_coroutines/ThrottleFirstLatestFlow.kt
+++ app/src/main/java/com/example/practicalblockkotlin/topic_2_coroutines/ThrottleFirstLatestFlow.kt
@@ -1,11 +1,10 @@
 package com.example.practicalblockkotlin.topic_2_coroutines
 
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.transform
 
 fun <T> Flow<T>.throttleFirst(windowDuration: Long): Flow<T> {
     if (windowDuration < 0) throw RuntimeException("Wrong window duration")
@@ -22,7 +21,10 @@ fun <T> Flow<T>.throttleFirst(windowDuration: Long): Flow<T> {
     }
 }
 
-@OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
-fun <T> Flow<T>.throttleLatest(windowDuration: Long): Flow<T> =
-    this.debounce(windowDuration)
-        .mapLatest { it }
+fun <T> Flow<T>.throttleLatest(windowDuration: Long): Flow<T> {
+    return this.conflate()
+        .transform {
+            emit(it)
+            delay(windowDuration)
+        }
+}
